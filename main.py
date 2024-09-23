524c4646071b0e0dfb0aa70efbf970b00e70994e
boulderingfreak <rsitek97@gmail.com>
2024-09-23 08:07:00 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hi in my temperature converter")
+
+choice = int(input("Type 1 to convert Celsiuses into Farenheits. \nType 2 to convert Farenheits into Celsiuses. \nYour choice: "))
+
+if choice == 1:
+    c = float(input("C: "))
+    result = (c * 9/5) + 32
+    print(f"{c} Celsiuses is {round(result, 1)} Farenheits.")
+elif choice == 2:
+    f = float(input("F: "))
+    result = (f - 32) * 5/9
+    print(f"{f} Farenheits is {round(result, 1)} Celsiuses.")
